8d062d8a935e99d1b50d184355233b069813fddd
Anthony M <47640864+IDeletedSystem64@users.noreply.github.com>
2021-02-06 11:18:51 -0600

    Fix userinfo from hanging the bot

---
+++ commands/userinfo.js
@@ -0,0 +1,64 @@
+module.exports = {
+
+  name: "userinfo",
+  description: "Gets info about an user, such as ID, Discord Join date and more.",
+  execute(client, message, args) {
+
+   
+
+    
+    
+    
+    const user = message.mentions.users.first();
+    const activity = user.presence.activities[i];
+
+    for (var i=0;i<user.presence.activities.length;i) {
+      if (activity.type == "CUSTOM_STATUS")
+        activity.name + activity.state
+        else
+        activity.type + activity.name
+    };
+
+    const embed = {
+      "title": "Everything you've ever wanted to know about " + user.username + "!",
+      "color": 172544,
+      "footer": {
+        "icon_url": "https://media.discordapp.net/attachments/549707869138714635/793524910172667964/Screenshot_26.png",
+        "text": "Made with ❤ in Illinois | Anitrox © 2018-2021 IDeletedSystem64"
+      },
+
+      "thumbnail": {
+        "url": user.displayAvatarURL()
+      },
+      "fields": [
+        {
+          "name": "Attention",
+          "value": "This command isn't yet finished, More info will be added soon!"
+        },
+        {
+          "name": "Full Username",
+          "value": user.tag
+        },
+        {
+          "name": "User Presence",
+          value: user.presence.status
+        },
+        {
+          "name": "User Status",
+           value:  activity.state + " " + playing.name
+        },
+        {
+          "name": "User ID",
+          "value": "``" + user.id + "``"
+        },
+        {
+          "name": "User Joined Discord",
+          "value": user.createdAt,
+          inline: true
+        },
+
+      ]
+    };
+    message.channel.send({ embed: embed });
+          }
+  }
