8938536120e21cd7418cd57a7480e9c372aa9a68
Anthony M <47640864+IDeletedSystem64@users.noreply.github.com>
2021-02-05 21:59:24 -0600

    Final 8ball, Thanks to @OfficialTCGMatt for helping with this.

--- commands/8ball.js
+++ commands/8ball.js
@@ -4,26 +4,27 @@ module.exports = {
     execute(client, message, args) {
         const answers = [
             "Heck no!",
-            "Are you crazy? No!",
+            "Are you crazy!? No!",
             "Don't even think about it.",
             "No! You might bork something!",
             "Heck yeah",
             "YEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE",
             "Definitely!",
             "Go for it! :smile:"
-        // This should have a 50/50 amount of yes and no answers for equality.
+        // This should have a 50/50 amount of yes and no answers for equality even though anitrox doesn't seem to care 🙃
         ]
         const index = Math.floor(Math.random() * (answers.length - 1) + 1);
-        const question = message.content
+        var question = args.slice(0).join(" ")
         var answer = (answers[index]);
+        console.log(args);
 
         const embed = {
             "title": ":8ball: Anitrox 8 Ball",
-            "description": "**" + question + "**",
+            "description": "Your question: **" + question + "**",
             "color": 6942950,
             "footer": {
-              "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
-              "text": "Made with :heart: in Illinois | Anitrox (C) IDeletedSystem64 2018-2021"
+                "icon_url": "https://images-ext-2.discordapp.net/external/-qaO3jaZLojhEnjrHiKABdXD7gLWqFvdUqHdskNGWhE/https/media.discordapp.net/attachments/549707869138714635/793524910172667964/Screenshot_26.png",
+                "text": "Made with ❤ in Illinois | Anitrox © 2018-2021 IDeletedSystem64"
             },
             
             "fields": [
