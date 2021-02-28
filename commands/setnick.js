6fd891b6365d2c8e404b50cc266e431a503122b7
Anthony M <47640864+IDeletedSystem64@users.noreply.github.com>
2021-02-28 00:39:52 -0600

    honestly. i dont know what im doing send help

--- commands/setnick.js
+++ commands/setnick.js
@@ -1,6 +1,13 @@
-//module.exports = {
-//    name: "setnick",
-//    description: "Set yours or another users nickname."
-//    execute(client, message, args) {
-        
-        
+const { DiscordAPIError } = require("discord.js")
+
+module.exports = {
+    name: "setnick",
+    description: "Set yours or another users nickname.",
+    execute(client, message, args) {
+        const guild = guild.fetch        
+        const member = guild.member(message.author)
+        if (guild.member.hasPermission('CHANGE_NICKNAME')) {
+            message.channel.send("yes")
+        } else {
+            message.channel.send("no")
+        }}}
