4935cf3d4326c09b54e17c2a2a56a1d8bf4f3358
TheCodingGuy <officialtcgmatt@gmail.com>
2021-03-19 00:50:30 -0400

    Fix setnick.js
    yeah.. I fixed it? yaay!?

--- commands/setnick.js
+++ commands/setnick.js
@@ -1,49 +1,49 @@
-const { GuildMember } = require("discord.js");
-
 module.exports = {
     name: 'setnick',
     description: 'Sets your nickname',
-    execute(client, message, args, Discord) {
+    execute(client, message, args) {
     
     if (message.channel.permissionsFor(message.author).has("CHANGE_NICKNAME")) {
-    var newnick = args.slice(0).join(" ")
+        var newnick = args.slice(0).join(" ")
 
-    try {
-    const successembed = {
-        "title": "<:AnitroxSuccess:809651936819019796> Nickname Changed",
-        "color": 9442302,
-        "footer": {
-          "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
-          "text": "Made with ❤ in Illinois | Anitrox © 2018-2021 IDeletedSystem64"
-        },
-        "fields": [
-          {
-            "name": "Changed nickname successfully!",
-            "value": "You need to have permission ``CHANGE_NICKNAME`` to change your nick!"
-          },
-          {
-            "name": "New Nickname",
-            "value": newnick,
-            "inline": true
-          }
-        ]
-      };
-      message.member.setNickname(newnick, "Nickname change requested by the server member. If you don't want users to be able to change their nickname disable 'CHANGE_NICKNAME' via Change Nickname in Roles.")
-    } catch (error) {
-        const failembed = {
-            "title": "<:AnitroxDenied:809651936642203668> Well that happened...",
-            "color": 13632027,
-            "footer": {
-              "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
-              "text": "Made with ❤ in Illinois | Anitrox © 2018-2021 IDeletedSystem64"
-            },
-            "fields": [
-              {
-                "name": "Failed to set nickname",
-                "value": "You need to have permission ``CHANGE_NICKNAME`` to change your nick!"
-              }
-            ]
-          };
-          message.channel.send({ failembed });
+        try {
+            const successembed = {
+                "title": "<:AnitroxSuccess:809651936819019796> Nickname Changed",
+                "color": 9442302,
+                "footer": {
+                    "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
+                    "text": "Made with ❤ in Illinois | Anitrox © 2018-2021 IDeletedSystem64"
+                },
+                "fields": [
+                    {
+                        "name": "Changed nickname successfully!",
+                        "value": "You need to have permission ``CHANGE_NICKNAME`` to change your nick!"
+                    },
+                    {
+                        "name": "New Nickname",
+                        "value": newnick,
+                        "inline": true
+                    }
+                ]
+            };
+            message.member.setNickname(newnick, "Nickname change requested by the server member. If you don't want users to be able to change their nickname disable 'CHANGE_NICKNAME' via Change Nickname in Roles.")
+            message.channel.send({ embed: successembed });
+        } catch (error) {
+            const failembed = {
+                "title": "<:AnitroxDenied:809651936642203668> Well that happened...",
+                "color": 13632027,
+                "footer": {
+                    "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
+                    "text": "Made with ❤ in Illinois | Anitrox © 2018-2021 IDeletedSystem64"
+                },
+                "fields": [
+                    {
+                        "name": "Failed to set nickname",
+                        "value": "You need to have permission ``CHANGE_NICKNAME`` to change your nick!"
+                    }
+                ]
+            };
+            message.channel.send({ embed: failembed });
+        };
     };
 }}}
