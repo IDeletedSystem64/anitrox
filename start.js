db5c9f4f551dc5706a35f8adbaaded0eb964a96e
Nathaniel Mason <nathanielma5on@yahoo.com>
2022-04-18 17:03:01 +0100

    Update indentation

--- start.js
+++ start.js
@@ -32,48 +32,48 @@ client.generateErrorMessage = (errorMsg, messageAuthorURL) => ({embed: {
 client.on("error", (e) => console.log(`[ERROR] ${error(e)}`));
 client.on("warn", (e) => (`[WARN] ${warn(e)}`));
 client.once('ready', () => {
-	console.clear()
-	console.log('    ___          _ __                 ');
-	console.log('   /   |  ____  (_) /__________  _  __');
-	console.log('  / /| | / __ \\/ / __/ ___/ __ \\| |/_/');
-	console.log(' / ___ |/ / / / / /_/ /  / /_/ />  <  ');
-	console.log('/_/  |_/_/ /_/_/\\__/_/   \\____/_/|_|  ');
-	console.log(`${config.release}, ${config.build}`);
-	console.log("Bot online. | Anitrox by IDeletedSystem64 | ALL MY CODE KEEPS BLOWING UP!");
-	// Statuses
-	setInterval(async () => {
-		// Picks a status from the config file
-		  const index = Math.floor(Math.random() * config.statuses.length);
-		  await client.user.setActivity(config.statuses[index]);
-	  }, 20000);
-	  
+  console.clear()
+  console.log('    ___          _ __                 ');
+  console.log('   /   |  ____  (_) /__________  _  __');
+  console.log('  / /| | / __ \\/ / __/ ___/ __ \\| |/_/');
+  console.log(' / ___ |/ / / / / /_/ /  / /_/ />  <  ');
+  console.log('/_/  |_/_/ /_/_/\\__/_/   \\____/_/|_|  ');
+  console.log(`${config.release}, ${config.build}`);
+  console.log("Bot online. | Anitrox by IDeletedSystem64 | ALL MY CODE KEEPS BLOWING UP!");
+  // Statuses
+  setInterval(async () => {
+    // Picks a status from the config file
+    const index = Math.floor(Math.random() * config.statuses.length);
+    await client.user.setActivity(config.statuses[index]);
+  }, 20000);
+    
 });
 
 
 // Begin Command Handler
 client.on('message', async (message) => {
 
-	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-	const args = message.content.slice(config.prefix.length).split(/\s+/);
-	const command = args.shift().toLowerCase();
-	
-	if (!client.commands.has(command)) return;
+  const args = message.content.slice(config.prefix.length).split(/\s+/);
+  const command = args.shift().toLowerCase();
+  
+  if (!client.commands.has(command)) return;
 
-	try {
-		await client.commands.get(command).execute(client, message, args, config);
-	} catch (error) {
-		console.stack;
-		message.channel.send({embed: {
-			"title": "<:AnitroxError:809651936563429416> **Something went wrong!**",
-			"description": error.stack,
-			"color": 13632027,
+  try {
+    await client.commands.get(command).execute(client, message, args, config);
+  } catch (error) {
+    console.stack;
+    message.channel.send({embed: {
+      "title": "<:AnitroxError:809651936563429416> **Something went wrong!**",
+      "description": error.stack,
+      "color": 13632027,
       "footer": {
         "icon_url": message.author.displayAvatarURL(),
         "text": config.footerTxt
       },
-        }});
-    }
+    }});
+  }
 });
 
 client.login(config.token);
