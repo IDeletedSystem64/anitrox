b817c6b997b5a80776e5895399b38542504ff891
IDeletedSystem64 <sylveon64@hotmail.com>
2022-11-09 23:27:13 -0600

    Undo changes to reload

--- commands/reload.js
+++ commands/reload.js
@@ -3,42 +3,43 @@ const { Constants } = require('discord.js');
 module.exports = {
 
   name: require('path').parse(__filename).name,
-  description: 'Reload a commands code',
-  options: [{
-    name: 'command',
-    description: 'The command you want to reload',
-    required: true,
+  description: 'Reloads a command',
+  options: [...Array(10).keys()].map(i => ({
+    name: `option${i + 0}`,
+    description: 'Another option',
+    required: i === 0,
     type: Constants.ApplicationCommandOptionTypes.STRING
-  }],
+  })),
 
   async parseInteraction (client, config, interaction) {
-    await interaction.reply(this.handle(client, config, interaction.user, interaction.options.getString('command')));
+    await interaction.reply(this.handle(client, config, interaction.user, [...Array(10).keys()].map(i => interaction.options.getString(`option${i + 1}`)).filter(str => str)));
   },
 
-  handle (client, __, user, args) {
+  handle (client, config, user, args) {
     if (user.id === process.env.OWNERID) {
       if (!args.length) return client.generateErrorMessage('You forgot to provide anything to reload, you pillock', user.displayAvatarURL());
       let returnMessage = '';
 
-      //      args.forEach(async (arg) => {
-      const commandName = args.toLowerCase();
-      const command = client.commands.get(commandName);
+      args.forEach(async (arg) => {
+        const commandName = arg?.toLowerCase();
+        const command = client.commands.get(commandName);
 
-      if (!command) {
-        returnMessage += `There is no command with name or alias \`${commandName}\`\n`;
-      } else {
-        delete require.cache[require.resolve(`./${command.name}.js`)];
+        if (!command) {
+          returnMessage += `There is no command with name or alias \`${commandName}\`\n`;
+        } else {
+          delete require.cache[require.resolve(`./${command.name}.js`)];
 
-        try {
-          const newCommand = require(`./${command.name}.js`);
-          client.commands.set(newCommand.name, newCommand);
-          returnMessage += `Successfully reloaded \`${commandName}\`\n`;
-          console.log(`User reloaded ${command.name}.`);
-        } catch (error) {
-          console.error(error);
-          returnMessage += `There was an error while reloading \`${command.name}\`\n`;
+          try {
+            const newCommand = require(`./${command.name}.js`);
+            client.commands.set(newCommand.name, newCommand);
+            returnMessage += `Successfully reloaded \`${commandName}\`\n`;
+            console.log(`User reloaded ${command.name}.`);
+          } catch (error) {
+            console.error(error);
+            returnMessage += `There was an error while reloading \`${command.name}\`\n`;
+          }
         }
-      }
+      });
 
       return returnMessage;
     } else {
